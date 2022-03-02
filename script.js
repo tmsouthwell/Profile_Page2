183a2d582b6d7f25246e1a332aa0effba177b469
tmsouthwell <goldtoneconsulting@gmail.com>
2022-03-01 19:07:32 -0800

    Likes

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeName(elementId ){
+    var element = document.querySelector(elementId)
+    element.innerText = "Jane Smith"
+}
+
+function hide(elementId){
+    var element = document.querySelector(elementId)
+    element.remove('#todd-e')
+}
+// function changeName(element){
+//     element.innerText = "Jane Smith";
+// }
